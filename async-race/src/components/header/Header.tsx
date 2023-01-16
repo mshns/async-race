80ed9c9d47fbd3a4a520fecc0051d81769d3a318
mshns <mishanos412@gmail.com>
2023-01-17 01:05:26 +0300

    feat: add header

--- async-race/src/components/header/Header.tsx
+++ async-race/src/components/header/Header.tsx
@@ -1,11 +1,15 @@
-import "./Header.scss"
+import "./Header.scss";
 
 function Header() {
   return (
     <header className="header">
-
+      <h1 className="header_logo">Async Race</h1>
+      <ul className="header_navigation">
+        <li className="navigation-item">Garage</li>
+        <li className="navigation-item">Winners</li>
+      </ul>
     </header>
-    )
+  );
 }
 
 export default Header;
